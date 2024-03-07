02a101e20933e2523fba7f30097110c7eed6d26d
Youness <younesssynester9@gmail.com>
2024-03-07 15:49:20 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        float firstNumberBielecki;
-        float secondNumberBielecki;
-        
+        float firstNumberBielecki,secondNumberBielecki,sum,difference, product, quotient;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:(and press Enter)");
+        firstNumberBielecki = scanner.nextFloat();
+
+        System.out.println("Enter the second number:(and press Enter)");
+        secondNumberBielecki = scanner.nextFloat();
+
+
+        sum = firstNumberBielecki + secondNumberBielecki;
+
+        difference = firstNumberBielecki - secondNumberBielecki;
+
+        product = firstNumberBielecki * secondNumberBielecki;
+
+        quotient = firstNumberBielecki / secondNumberBielecki;
+
+        System.out.printf("Sum: %.2f\n", sum);
+
+        System.out.printf("Difference: %.2f\n", difference);
+
+        System.out.printf("Product: %.2f\n", product);
+
+        System.out.printf("Quotient: %.2f\n", quotient);
+
+
+
     }
 }
